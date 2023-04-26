3de94b9a9b1acb7218e45183e84741b8acf57c68
uzma1411 <47413610+uzma1411@users.noreply.github.com>
2023-04-26 23:45:49 +0530

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,107 @@
+AFRAME.registerComponent("bullets", {
+  init: function () {
+    this.shootBullet();
+  },
+  shootBullet: function () {
+    window.addEventListener("keydown", (e) => {
+      if (/** write the condition to check z key */) {
+        var bullet = document.createElement("a-entity");
+
+        bullet.setAttribute("geometry", {
+          primitive: "sphere",
+          radius: 0.1,
+        });
+
+        bullet.setAttribute("material", "color", "black");
+
+        var cam = document.querySelector("#camera");
+
+        pos = cam.getAttribute("position");
+
+        bullet.setAttribute("position", {
+          x: pos.x,
+          y: pos.y,
+          z: pos.z,
+        });
+
+        var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
+       
+        //set the velocity and it's direction
+       
+        var scene = document.querySelector("#scene");
+
+        
+        bullet.setAttribute("dynamic-body", {
+          shape: "sphere",
+          mass: "0",
+        });
+        bullet.setAttribute("visible", false);
+
+        //add the collide event listener to the bullet
+        
+
+        scene.appendChild(bullet);
+
+        this.shootSound();
+      }
+    });
+  },
+  removeBullet: function (e) {
+    var scene = document.querySelector("#scene");
+    //bullet element
+    var element = e.detail.target.el;
+
+    //element which is hit
+    var elementHit = e.detail.body.el;
+
+    //Create paint splash
+    var paint = document.createElement("a-entity");
+    var pos = element.getAttribute("position")
+    var rotate = elementHit.getAttribute("rotation")
+
+    paint.setAttribute("position", {
+      x: pos.x,
+      y: pos.y,
+      z: pos.z,
+    });
+    paint.setAttribute("rotation", {
+      x: rotate.x,
+      y: rotate.y,
+      z: rotate.z,
+    });
+    paint.setAttribute("scale", {
+      x: 2,
+      y: 2,
+      z: 2,
+    });
+
+
+    var colorNum = parseInt(Math.random() * 8 + 1)
+
+    paint.setAttribute("material", {
+      opacity: 1,
+      transparent: true,
+      src: "./images/paint splash-0" + colorNum + ".png"
+    });
+
+    paint.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.5,
+      height: 0.5
+    });
+    scene.appendChild(paint)
+
+    //remove event listener
+    element.removeEventListener("collide", this.removeBullet);
+
+    //remove the bullets from the scene      
+    scene.removeChild(element);
+  },
+  shootSound: function () {
+    var entity = document.querySelector("#sound1");
+    entity.components.sound.playSound();
+  },
+});
+
